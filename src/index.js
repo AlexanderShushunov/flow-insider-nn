44a1efe864383ad1e7674c7938627caa8c03dacd
Alexandr Shushunov <ashushunov@mail.ru>
2017-10-13 16:57:48 +0300

    Дотипизировал все

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import {App} from './app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+if (root) {
+    ReactDOM.render(<App />, root);
+}
 registerServiceWorker();
